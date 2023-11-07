b1e3e3fdd7335898ac537d73f3bc618c5e364669
Koenv <koenvanvlimmeren@gmail.com>
2023-11-07 11:14:25 +0100

    Added Admin indicator

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -37,7 +37,11 @@
                         <x-slot name="trigger">
                             <button
                                 class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                                <div>{{ Auth::user()->name }}</div>
+                                <div>{{ Auth::user()->name }}
+                                    @if (Auth::user()->role == 'admin')
+                                        <i class="fas fa-crown"></i> <!-- This is the Font Awesome icon for a crown -->
+                                    @endif
+                                </div>
 
                                 <div class="ml-1">
                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
@@ -58,7 +62,7 @@
 
                             @if (Auth::user()->role == 'admin')
                                 <x-dropdown-link :href="route('homepage')">
-                                {{ __('Admin Panel') }}
+                                    {{ __('Admin Panel') }}
                                 </x-dropdown-link>
                             @endif
 
@@ -123,7 +127,11 @@
             <!-- Responsive Settings Options -->
             <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
                 <div class="px-4">
-                    <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
+                    <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}
+                        @if (Auth::user()->role == 'admin')
+                            <i class="fas fa-crown"></i> <!-- This is the Font Awesome icon for a crown -->
+                        @endif
+                    </div>
                     <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
                 </div>
 
