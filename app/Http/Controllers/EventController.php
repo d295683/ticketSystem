d54b44b4efe4064a1466db4c0ba395ec3b24a4ec
Koenv <koenvanvlimmeren@gmail.com>
2023-11-07 11:54:16 +0100

    Refactor event filtering in EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -12,22 +12,11 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
-        // Get all events from the database and order them by datetime
-        $events = Event::orderBy('datetime', 'asc')->get()
-            // Group events by year
+        $events = Event::where('datetime', '>=', now())
+            ->orderBy('datetime', 'asc')
+            ->get()
             ->groupBy(function ($event) {
                 return $event->datetime->format('Y');
-            })
-            // Map through each year group and filter out events that are not today or in the future
-            ->map(function ($yearGroup) {
-                return $yearGroup->filter(function ($event) {
-                    return $event->datetime->isToday() || $event->datetime->isFuture();
-                });
-            })
-            // Filter out year groups that are empty
-            ->filter(function ($yearGroup) {
-                return $yearGroup->isNotEmpty();
             });
 
         return view('events.index', compact('events'));
