a3dc1070d86c36e8a92bc84d671f085702672015
Koenv <koenvanvlimmeren@gmail.com>
2023-12-08 10:02:28 +0100

    Rename api routes to api.*

--- routes/api.php
+++ routes/api.php
@@ -22,24 +22,27 @@ use App\Http\Controllers\API\TicketController;
 /**
  * Auth routes
  */
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::delete('/logout', [AuthController::class, 'logout']);
-    Route::get('/refresh', [AuthController::class, 'refresh']);
-});
+Route::name('api.')->group(function () {
+
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::delete('/logout', [AuthController::class, 'logout']);
+        Route::get('/refresh', [AuthController::class, 'refresh']);
+    });
 
 
-Route::prefix('/events')->name('events.')->group(function () {
-    Route::get('/', [EventController::class, 'index'])->name('index');
-    Route::get('/{event}', [EventController::class, 'show'])->name('show');
-});
+    Route::prefix('/events')->name('api.events.')->group(function () {
+        Route::get('/', [EventController::class, 'index'])->name('index');
+        Route::get('/{event}', [EventController::class, 'show'])->name('show');
+    });
 
-Route::middleware(['auth:sanctum', 'role:admin'])->post('/tickets/scan', [TicketController::class, 'update']);
+    Route::middleware(['auth:sanctum', 'role:admin'])->post('/tickets/scan', [TicketController::class, 'update']);
 
-Route::prefix('/me')->middleware(['auth:sanctum'])->group(function () {
-    Route::get('/', [UserController::class, 'index']);
-    Route::get('/roles', [UserController::class, 'roles']);
-    Route::get('/reservations', [UserController::class, 'reservations']);
-    Route::get('/reservations/{reservation}', [UserController::class, 'reservation']);
+    Route::prefix('/me')->middleware(['auth:sanctum'])->group(function () {
+        Route::get('/', [UserController::class, 'index']);
+        Route::get('/roles', [UserController::class, 'roles']);
+        Route::get('/reservations', [UserController::class, 'reservations']);
+        Route::get('/reservations/{reservation}', [UserController::class, 'reservation']);
+    });
 });
